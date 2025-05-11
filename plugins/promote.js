13656bb68d057d387817ced21ab2a978d96f700d
Ayodele David <githubworker5@gmail.com>
2025-05-11 19:59:04 +0100

    Update promote.js

--- plugins/promote.js
+++ plugins/promote.js
@@ -15,8 +15,10 @@ module.exports = {
       const isBotAdmin = admins.find(admin => admin.user.id === bot.botInfo.id);
       const isSenderAdmin = admins.find(admin => admin.user.id === msg.from.id);
 
+    /*
       if (!isBotAdmin) return bot.sendMessage(chatId, "I need to be an admin to promote someone.");
       if (!isSenderAdmin) return bot.sendMessage(chatId, "You must be an admin to use this command.");
+   */
 
       await bot.promoteChatMember(chatId, parseInt(userId), {
         can_change_info: true,
